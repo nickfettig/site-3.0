ec2bbb5614bdbd6c5ca784678423bbf82307c976
Nick Fettig <77179593+nickfettig@users.noreply.github.com>
2025-11-05 11:13:08 -0500

    people made fun of me for the landing page

--- src/components/common/navlink.tsx
+++ src/components/common/navlink.tsx
@@ -12,8 +12,8 @@ export default function NavLink({text, link}: {text: string, link: string}) {
 
     let id_link = link.split(':');
     if (id_link[0] === 'id') {
-        return <p className="cursor-pointer hover:text-gray-700" onClick={() => scrollToSection(id_link[1])}>{text}</p>
+        return <p className="cursor-pointer hover:text-[#382E47]" onClick={() => scrollToSection(id_link[1])}>{text}</p>
     }
 
-    return <Link className="cursor-pointer hover:text-gray-700" href = {link}>{text}</Link>
+    return <Link className="cursor-pointer hover:text-[#382E47]" href = {link}>{text}</Link>
 }
--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -4,18 +4,18 @@ import NavLink from "./common/navlink";
 export default function Landing() {
   return (
     <div className = 'w-full h-screen landing py-8 px-12 overflow-hidden relative mx-auto'>
-      <div className={`text-landing-nav ${space.className}  text-xl md:text-2xl 2xl:text-3xl flex items-start justify-center lg:justify-between font-light max-w-7xl mx-auto`}>
+      <div className={`text-[#382E47]/60 ${space.className}  text-xl md:text-2xl 2xl:text-3xl flex flex-col lg:flex-row items-center lg:items-start lg:justify-between font-light max-w-7xl mx-auto`}>
         <NavLink text = "fettig.dev" link = "https://fettig.dev" />
+        <div>
+          <p className={`text-center ${sora.className} text-[#382E47]/90 text-4xl md:text-5xl font-light`}>NICK FETTIG</p>
+          <p className = {`text-center ${space.className} text-[#382E47]/60 text-lg md:text-2xl font-light`}>Student & Developer</p>
+        </div>
         <div className="flex-col gap-4 text-right hidden lg:flex z-10">
             <NavLink text = "Portfolio" link = "id:portfolio" />
             <NavLink text = "About" link = "id:about" />
             <NavLink text = "Contact" link = "id:contact" />
             <NavLink text = "Blog" link = "blog" />
         </div>
-      </div>
-      <div className="xl:bg-white/10 rounded-full text-center width-custom absolute top-1/12 xl:top-5/6 2xl:top-5/6 left-1/2 -translate-x-1/2 xl:-translate-y-1/2">
-        <p className={`text-center ${sora.className} text-[#516765] text-4xl md:text-5xl font-light mt-10`}>NICK FETTIG</p>
-        <p className = {`text-center ${space.className} text-[#598282] text-lg md:text-2xl font-light`}>Student & Developer</p>
       </div>
     </div>
   );
